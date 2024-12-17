68bed6ed202374fb0604befd16f69de3d3409df2
MKA <MKA@A26PC5>
2024-12-17 20:45:08 +0200

    Add project files.

---
+++ 05_ht and practise job/Vector.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ht_and_practise_job
+{
+    internal class Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Vector() 
+            : this(0, 0) { }
+
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+
+        public double Length
+        {
+            get { return Math.Sqrt(X * X + Y * Y); }
+        }
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.X + a.X, b.Y + b.Y);
+        }
+        public static Vector operator -(Vector a, Vector b)
+        {
+            return new Vector(a.X - a.X, b.Y - b.Y);
+        }
+
+        public static Vector operator *(Vector v, double num)
+        {
+            return new Vector(v.X * num, v.Y * num);
+        }
+
+        public static double operator *(Vector a, Vector b) 
+        {
+            return a.X * b.X + a.Y * b.Y;
+        }
+
+        public static bool operator ==(Vector a, Vector b)
+        {
+            return a.X == b.X && a.Y == b.Y;
+        }
+
+        public static bool operator !=(Vector a, Vector b)
+        {
+            return !(a == b);
+        }
+
+         
+    }
+}
---
+++ 05_null_operator/Program.cs
@@ -0,0 +1,65 @@
+﻿internal class Program
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string lastName { get; set; }
+    }
+
+    private static void Main(string[] args)
+    {
+        string str = null;
+        //int value = null;
+        Console.WriteLine(default(string) == null);
+        Console.WriteLine(default(int[]) == null);
+        Console.WriteLine(default(int));
+        Console.WriteLine(default(bool));
+        //Console.WriteLine(value);
+        /*if(str == null)
+        {
+            Console.WriteLine("Default text");
+        }
+        else
+        {
+            Console.WriteLine(str);
+        }*/
+
+        Console.WriteLine();
+        // унарний оператор
+        //++і
+        //!true
+        //-і
+        //бінарний оператор
+        // a + b + c
+        // a == b
+        // тернариний оператор
+        // a ? b : c
+
+        str = "Lorem ipsum";
+        Console.WriteLine(str != null ? str : "Default text");
+        // ?? 
+        Console.WriteLine(str ?? "Default text");
+
+        //Person p = new Person() {Name = null };
+        //Person p = new Person() {Name = "Denis" };
+
+        // ?
+
+        //Person p = null;
+        //Console.WriteLine(p?.Name ?? "None name");
+
+
+        Console.WriteLine(str?.Length ?? -1);
+        Console.WriteLine(str?.Replace('m', '@') ?? "Error");
+
+        Nullable<int> value = null;
+        Console.WriteLine(value.GetValueOrDefault());
+        int? number = null;
+
+        int[] arr = null;
+        Console.WriteLine(arr?[0] ?? -1);
+
+        arr ??= new int[6] { 1, 2, 3, 4, 5, 6 };
+        Console.WriteLine(arr?[0] ?? -1);
+    }
+}
